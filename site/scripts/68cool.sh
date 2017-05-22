2208b48ad0a15361263c4b0d458a7a9da5807cc8
Luke Schimmel <lbs.lic@me.com>
2017-05-22 03:46:25 +0000

    added more air conditioner scripts

---
+++ site/scripts/68cool.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+irsend SEND_ONCE aircon KEY_POWER
+wait 0.1
+irsend SEND_ONCE aircon KEY_MODE
+wait 0.1
+irsend SEND_ONCE aircon KEY_MODE
+wait 0.1
+irsend SEND_ONCE aircon KEY_F
+wait 0.1
+irsend SEND_ONCE aircon KEY_F
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_DOWN
+wait 0.1
+irsend SEND_ONCE aircon KEY_UP
+wait 0.1
+irsend SEND_ONCE aircon KEY_UP
+wait 0.1
+irsend SEND_ONCE aircon KEY_UP
+wait 0.1
+irsend SEND_ONCE aircon KEY_UP
+
